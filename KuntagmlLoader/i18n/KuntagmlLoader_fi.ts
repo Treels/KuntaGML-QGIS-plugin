1facc4cc299035b5c94e235f379c81ed362a06d6
Joonalai <joona@gispo.fi>
2020-05-26 12:13:10 +0300

    Fix initial tests

---
+++ KuntagmlLoader/i18n/KuntagmlLoader_fi.ts
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0" language="fi" sourcelanguage="en">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="../test/test_translations.py" line="48"/>
+        <source>Good morning</source>
+        <translation>Hyvää huomenta</translation>
+    </message>
+</context>
+</TS>
