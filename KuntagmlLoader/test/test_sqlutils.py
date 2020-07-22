7a64328e6020418c639d76fe3f52c682fe67acac
Joonalai <joona@gispo.fi>
2020-07-22 14:26:39 +0300

    Skip tests using local test files

--- KuntagmlLoader/test/test_sqlutils.py
+++ KuntagmlLoader/test/test_sqlutils.py
@@ -1,14 +1,21 @@
 import os
 
+import pytest
+
 from ..core.utils.sql_utils import SQLTable, get_non_empty_tables
+from ..qgis_plugin_tools.testing.utilities import is_running_inside_ci
 
 
+# TODO: create proper test files
+@pytest.mark.skipif(is_running_inside_ci(), reason="CI")
 def test_getting_tables(new_project) -> None:
     db = os.path.join(os.path.dirname(__file__), "testdata", "test.sqlite")
     tables = get_non_empty_tables(db)
     assert len(tables) == 5
 
 
+# TODO: create proper test files
+@pytest.mark.skipif(is_running_inside_ci(), reason="CI")
 def test_getting_tables2() -> None:
     db = os.path.join(os.path.dirname(__file__), "testdata", "kanta_Kevyenliikenteenvayla.sqlite")
     tables = get_non_empty_tables(db)
